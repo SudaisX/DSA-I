b4eb4cf09daa5406b0075ae9ec2b0420e833896b
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-02-14 18:14:48 +0500

    updated assignment 1

---
+++ Assignment 1/Gauss Jordan Method.py
@@ -0,0 +1,39 @@
+import ast
+coeffs = input()
+coeffs = ast.literal_eval(coeffs)
+
+def multiply_row(row, n):
+    return [element*n for element in row]
+def add_row(row, n):
+    return [element+n for element in row]
+def add_rows(row1, row2):
+    return [row1[i]+row2[i] for i in range(len(row1))]
+
+def Solve(coeffs):
+    length  = len(coeffs)
+    
+    #swap until the first element is not 0
+    while coeffs[0][0] == 0:
+        coeffs.append(coeffs.pop(0))
+            
+    for i in range(length):
+        #turn the pivot into 1
+        if coeffs[i][i] != 1:
+            try:
+                coeffs[i] = multiply_row(coeffs[i], 1/coeffs[i][i])
+            except ZeroDivisionError:
+                coeffs[i] = add_row(coeffs[i], 1)
+
+        #turn the rows below the pivot into 0
+        for j in range(i+1, length):
+            if coeffs[j][i] != 0:
+                coeffs[j] = add_rows(coeffs[j], multiply_row(coeffs[i], -coeffs[j][i]))
+
+        #turn the rows above the pivot into 0
+        for j in range(i):
+            if coeffs[j][i] != 0:
+                coeffs[j] = add_rows(coeffs[j], multiply_row(coeffs[i], -coeffs[j][i]))
+                    
+    return [round(x[-1], 1) for x in coeffs] 
+   
+print(Solve(coeffs))
---
+++ Quizes/HTML Balance.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self):
+        self.stack = []   
+    def push(self, item):
+        self.stack.append(item)
+    def pop(self):
+        return self.stack.pop()
+    def is_empty(self):
+        return self.stack == []
+    def top(self):
+        return self.stack[-1]
+
+#vvvv similar to balanced braces
+def balanced_braces(string):
+    stack = Stack()
+    for s in string:
+        if s == '{': stack.push('}')
+        elif s == '[': stack.push(']')
+        elif s == '(': stack.push(')')
+        else:
+            if stack.is_empty() or stack.top() != s:
+                return False
+            else:
+                stack.pop()
+    return stack.is_empty()
+
+string = input('Enter braces: ')
+print(balanced_braces(string))
