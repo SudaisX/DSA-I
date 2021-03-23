635cf7f133153044e37def19e519f127647cefef
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-03-23 13:28:38 +0500

    Update graph.py

--- Lab 9 (Graphs)/graph.py
+++ Lab 9 (Graphs)/graph.py
@@ -80,7 +80,10 @@ class Graph:
         return nearest_node
 
     def isNeighbour(self, node1, node2):
-        pass #too lazy
+        for edges in self.graph[node1]:
+            if edges[0] == node2:
+                return True
+        return False
 
     def removeNode(self, node):
         self.graph.pop(node)
@@ -101,17 +104,22 @@ class Graph:
         return self.graph
 
     def display_adj_matrix(self):
-        pass
+        v = len(self.graph)
+        matrix = [[0 for i in range(v)] for j in range(v)]
+        for node in self.graph:
+            for edges in self.graph[node]:
+                matrix[node][edges[0]] = edges[1]
+        return matrix
 
 
 G = Graph()
 
 
 #G.graph = { 0: [(1, 1), (2, 1)], 1: [(2, 1), (3, 1)], 2: [(4, 1)], 3: [(4, 1), (5, 1)], 4: [(5, 1)], 5: [] }
-G.graph = { 0: [(1, 1), (2, 1)], 1: [(0, 1), (2, 1), (3, 1)], 2: [(0, 1), (1, 1), (4, 1)], 3: [(1, 1), (4, 1), (5, 1)], 4: [(3, 1), (2, 1), (5, 1)], 5: [(3, 1), (4, 1)] }
+#G.graph = { 0: [(1, 1), (2, 1)], 1: [(0, 1), (2, 1), (3, 1)], 2: [(0, 1), (1, 1), (4, 1)], 3: [(1, 1), (4, 1), (5, 1)], 4: [(3, 1), (2, 1), (5, 1)], 5: [(3, 1), (4, 1)] }
 #G.graph = { 0: [(1, 21), (2, 15)], 1: [(0, 21), (2, 10), (3, 70)], 2: [(0, 15), (1, 10), (4, 50)], 3: [(1, 70), (4, 24), (5, 39)], 4: [(3, 24), (2, 50), (5, 99)], 5: [(3, 39), (4, 99)] }
+#G.graph = { 0: [(1, 21), (2, 15)], 1: [(2, 10), (3, 70)], 2: [(4, 50)], 3: [(4, 24), (5, 39)], 4: [(5, 99)], 5: [] }
 
-pprint(G.displayGraph())
-G.removeNodes([1, 2])
-#G.removeNode(2)
-pprint(G.displayGraph())
+
+#pprint(G.displayGraph())
+#pprint(G.display_adj_matrix())
