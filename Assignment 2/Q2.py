701f9b313f2a3aa5fcffefc56782d3819310b347
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-03-31 15:10:35 +0500

    Update Q2.py

--- Assignment 2/Q2.py
+++ Assignment 2/Q2.py
@@ -1,28 +1,3 @@
-# def MergeSortandCountInversions(lst) :
-#     if len(lst) < 2:
-#         return lst, 0
-        
-#     mid = len(lst)//2
-#     left, left_i = MergeSortandCountInversions(lst[:mid])
-#     right, right_i = MergeSortandCountInversions(lst[mid:])
-#     result = []
-
-#     i, j = 0, 0
-#     inversions = left_i + right_i
-
-#     while i < len(left) and j < len(right):
-#         if left[i] < right[j]:
-#             result.append(left[i])
-#             i += 1
-#         else:
-#             result.append(right[j])
-#             j += 1
-#             inversions += (mid - i)
-
-#     result += left[i:]
-#     result += right[j:]
-#     return result, inversions
-
 def CountInversions(lst):
     def MergeSortandCountInversions(lst) :
         if len(lst) < 2:
@@ -31,11 +6,11 @@ def CountInversions(lst):
         mid = len(lst)//2
         left, left_i = MergeSortandCountInversions(lst[:mid])
         right, right_i = MergeSortandCountInversions(lst[mid:])
+        
         result = []
-
         i, j = 0, 0
         inversions = left_i + right_i
-
+        
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
                 result.append(left[i])
