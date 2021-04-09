1011659d7778e0856be2b0d28947ad7ebcfe4e94
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-04-09 13:32:52 +0500

    Update Merge Sort.py

--- Sorting/Merge Sort.py
+++ Sorting/Merge Sort.py
@@ -1,31 +1,31 @@
-def merge(left, right):
-    result = []
-    i,j = 0,0
+# def merge(left, right):
+#     result = []
+#     i,j = 0,0
 
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    while i < len(left):
-        result.append(left[i])
-        i += 1
-    while j < len(right):
-        result.append(right[j])
-        j += 1
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             result.append(left[i])
+#             i += 1
+#         else:
+#             result.append(right[j])
+#             j += 1
+#     while i < len(left):
+#         result.append(left[i])
+#         i += 1
+#     while j < len(right):
+#         result.append(right[j])
+#         j += 1
 
-    return result
+#     return result
 
-def merge_sort(lst):
-    if len(lst) < 2:
-        return lst
-    else:
-        middle = len(lst)//2
-        left = merge_sort(lst[:middle])
-        right = merge_sort(lst[middle:])
-        return merge(left, right)
+# def merge_sort(lst):
+#     if len(lst) < 2:
+#         return lst
+#     else:
+#         middle = len(lst)//2
+#         left = merge_sort(lst[:middle])
+#         right = merge_sort(lst[middle:])
+#         return merge(left, right)
         
 
 
