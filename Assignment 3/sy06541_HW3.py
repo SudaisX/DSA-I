9a4f9387e3f2668944b2b10170dc9fb9cb3c2563
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-04-30 21:02:20 +0500

    Update sy06541_HW3.py

--- Assignment 3/sy06541_HW3.py
+++ Assignment 3/sy06541_HW3.py
@@ -34,6 +34,41 @@ def add_connection(network, user_A, user_B):
     elif user_B in network[user_A][0]:
         return network
 
+def add_new_user(network, user, countries):
+    if user not in network:
+        network[user] = [[], countries]
+        return network
+    return network
+
+def get_secondary_connections(network, user):
+    if user not in network:
+        return None
+    if network[user][0] == []:
+        return []
+
+    secondary_connections = []
+    for connection in network[user][0]:
+        if connection not in secondary_connections:
+            secondary_connections.append(connection)
+    return secondary_connections
+
+def count_common_connections(network, user_A, user_B):
+    if user_A not in network or user_B not in network:
+        return False
+
+    common = []
+    for connA in network[user_A][0]:
+        for connB in network[user_B][0]:
+            if connA == connB and connA not in common and connB not in common:
+                common.append(connA)
+    return len(common)
+
+
+def find_path_to_patient(network, user_A, user_B):
+	# your RECURSIVE solution here!
+	return None
+
+
 
 data = "Usama is connected to Saeed, Aaliya, Mohsin.Usama traveled to Italy, Japan, Korea.Saeed is connected to Sumaira, Zehra, Samar, Marium.Saeed traveled to China, Afghanistan.Marium is connected to Mohsin, Kashif, Saeed.Marium traveled to Japan, USA, Iran.Sumaira is connected to Usama, Zehra.Sumaira traveled to Japan, Saudi Arabia.Aaliya is connected to Mohsin, Bari, Sameera, Kashif.Aaliya traveled to India, USA, Malaysia.Mohsin is connected to Usama, Bari, Saeed.Mohsin traveled to Iran, Indonesia, Afghanistan.Bari is connected to Zehra, Usama, Mohsin.Bari traveled to Japan, India, China.Zehra is connected to Marium, Samar, Saeed.Zehra traveled to Russia, Malaysia, Italy.Sameera is connected to Bari, Usama, Samar, Kashif.Sameera traveled to Afghanistan, Korea, Russia.Kashif is connected to Zehra.Kashif traveled to Russia, Malaysia.Samar is connected to Sumaira, Usama, Aaliya.Samar traveled to Saudi Arabia, Indonesia, Iran."
 network = create_data_structure(data)
@@ -45,4 +80,4 @@ print()
 # pprint(get_countries_traveled(network, 'Mohsin'))
 # print()
 
-pprint(add_connection(network, 'Mohsin', 'Samar'))
+pprint(count_common_connections(network, 'Mohsin', 'Usama'))
